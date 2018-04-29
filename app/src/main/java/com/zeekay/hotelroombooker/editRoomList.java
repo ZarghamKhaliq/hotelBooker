fc1eb6c96095e78baa8efa9599818bafc2d4f987
huzaifah <zarghamkhaliqcs@gmail.com>
2018-04-29 22:01:57 +0500

    Edit Room

---
+++ app/src/main/java/com/zeekay/hotelroombooker/editRoomList.java
@@ -0,0 +1,13 @@
+package com.zeekay.hotelroombooker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editRoomList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_room_list);
+    }
+}
